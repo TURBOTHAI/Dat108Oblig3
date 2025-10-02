c1a3865fc3c523f1561e6d121238df73ec7a36cd
798118 <164620581+TURBOTHAI@users.noreply.github.com>
2025-10-02 21:21:25 +0200

    JAAAAAAAAAAAAAAAAAAAAAAAAAA

---
+++ DAT108OBLIG3/src/main/java/oblig3/DAT108OBLIG3/Dat108Oblig3Application.java
@@ -0,0 +1,13 @@
+package oblig3.DAT108OBLIG3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dat108Oblig3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dat108Oblig3Application.class, args);
+	}
+
+}
---
+++ DAT108OBLIG3/src/test/java/oblig3/DAT108OBLIG3/Dat108Oblig3ApplicationTests.java
@@ -0,0 +1,13 @@
+package oblig3.DAT108OBLIG3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dat108Oblig3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
